d7a9ddf15b63f2f2e5c4357dbc212b90db3c78cd
BarZu <cjlice@qq.com>
2017-02-03 15:15:01 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # JTaro-Bundle
-为JTaro服务的打包脚本
+
+为JTaro服务的打包脚本，将整个文件夹所有js文件（Vue页面组件）合并到一个文件并创建Vue组件
+
+```js
+// nodejs
+var jtaroBundle = require('jtaro-bundle')
+jtaroBundle({
+  src: 'src/pages',  // 源目录
+  dest: 'src/pages.js'  // 目标文件
+})
+```
---
+++ index.js
@@ -0,0 +1,15 @@
+var fs = require('fs')
+var path = require('path')
+
+exports = function (src, dest) {
+  // 遍历demos/jroll_demo/pages里的js文件路径创建bundle.js
+  var paths = fs.readdirSync(path.resolve(src))
+  var content = ''
+
+  paths.forEach((item, index) => {
+    if (/\.js$/.test(item)) {
+      content += 'import p' + index + ' from \'./pages/' + item + '\'\nVue.component(\'pages__' + item.replace('.js', '') + '\', p' + index + ')\n'
+    }
+  })
+  fs.writeFileSync(path.resolve(dest), content)
+}
