ec0a9c1ddef2df6deeef9863b13cd18c142812ae
BarZu <1419328765@qq.com>
2017-02-24 16:47:59 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,12 +1,39 @@
 # JTaro-Bundle
 
-为JTaro服务的打包脚本，将整个文件夹所有js文件（Vue页面组件）合并到一个文件并创建Vue组件
+为JTaro服务的打包脚本
+
+## 作用
+
+- 根据index.html的href和src属性提取需要的文件
+- 将整个文件夹所有js文件（Vue页面组件）合并到一个文件并创建Vue组件
+- 直接拷贝指定文件（夹）
+
+## 安装
+
+```bash
+npm install -D jtaro-bundle
+```
+
+## 用法
 
 ```js
 // nodejs
 var jtaroBundle = require('jtaro-bundle')
+var uglify = require('rollup-plugin-uglify')
+
 jtaroBundle.bundle({
-  src: 'src/pages',  // 源目录
-  dest: 'src/pages.js'  // 目标文件
+  rollupPlugins: [uglify()],  // 自定义使用rollup打包时使用的插件
+  origin: 'dev/index.html',  // 开发目录的index.html
+  target: 'pro/index.html',  // 生产目录的index.html
+  copies: ['./assets', './data.json']  // 直接拷贝的文件（夹）
 })
 ```
+
+## 选项
+
+| 键 | 值 | 必填 | 备注 |
+|:--:|:--:|:--:|:--:|
+| origin | <String> | Y | 开发目录的index.html |
+| target | <String> | Y | 生产目录的index.html |
+| copies | <Array> | N | 直接拷贝的文件（夹） |
+| rollupPlugins | <Array> | N | 参考 [Rollup JavaScript-API#plugins](https://github.com/rollup/rollup/wiki/JavaScript-API#plugins) |
--- index.js
+++ index.js
@@ -1,15 +1,86 @@
-var fs = require('fs')
+var fs = require('fs-extra')
 var path = require('path')
+var rollup = require('rollup')
+var jtaroModule = require('rollup-plugin-jtaro-module')
 
+/**
+ * 扫描目标index.html，从相对源index.html提取需要的文件
+ * @param options.origin String 开发版的index
+ * @param options.target String 生产版的index
+ * @param options.copies Array<String> 不能从目标index扫描到的文件从这里拷贝，元素可为目录或文件，都必须相对源index
+ */
 exports.bundle = function (options) {
+  // 扫描目标index
+  var assets = scanTarget(options.target)
+  var originDir = path.dirname(options.origin)
+  var targetDir = path.dirname(options.target)
+
+  // 拷贝需要直接拷贝的文件，合并不存在的文件（将文件夹里所有html/js用rollup打包成一个与文件夹同名的js文件）
+  copyFiles(assets.concat(options.copies), originDir, targetDir, options.rollupPlugins || [])
+}
+
+function copyFiles (copies, ori, tar, plugins) {
+  copies.forEach(f => {
+    const oriFile = path.resolve(ori, f)
+    const tarFile = path.resolve(tar, f)
+    fs.stat(oriFile, (err, s) => {
+      if (err) {
+        fs.stat(path.dirname(oriFile), (err, s) => {
+          if (s.isDirectory()) {
+            rollupBundle({
+              root: ori,
+              src: oriFile.replace(/\.\w+/, '/'),
+              dest: tarFile,
+              plugins: plugins
+            })
+          }
+          if (err) throw err
+        })
+      } else if (s.isFile() || s.isDirectory()) {
+        fs.copy(oriFile, path.resolve(tar, f), err => {
+          if (err) throw err
+        })
+      }
+    })
+  })
+}
+
+function scanTarget (file) {
+  var text = fs.readFileSync(path.resolve(file)).toString()
+  var paths = []
+  text.replace(/(?:(?:href|src)=(?:"|'))([^"']+)(?:"|')/g, (match, p) => {
+    paths.push(p)
+  })
+  return paths
+}
+
+function rollupBundle (options) {
   // 遍历demos/jroll_demo/pages里的js文件路径创建bundle.js
   var paths = fs.readdirSync(path.resolve(options.src))
   var content = ''
+  var tempFile = path.resolve(options.root || '', '_jtaro_bundle_temp_' + path.basename(options.dest))
 
   paths.forEach((item, index) => {
     if (/\.js$/.test(item)) {
       content += 'import p' + index + ' from \'./pages/' + item + '\'\nVue.component(\'pages__' + item.replace('.js', '') + '\', p' + index + ')\n'
     }
   })
-  fs.writeFileSync(path.resolve(options.dest), content)
+  // 创建临时文件
+  fs.writeFileSync(tempFile, content)
+
+  rollup.rollup({
+    entry: tempFile,
+    context: 'window',
+    plugins: [jtaroModule({
+      root: options.root || ''
+    })].concat(options.plugins)
+  }).then(function (bundle) {
+    bundle.write({
+      format: 'iife',
+      dest: path.resolve(options.dest)
+    })
+    // 删除临时文件
+    fs.unlinkSync(tempFile)
+  })
 }
+
